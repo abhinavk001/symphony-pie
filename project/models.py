d03f066cf3c26f94eb8d8baed84f4fc9d2248564
sarayu-suresh <sarayusuresh888@gmail.com>
2021-05-19 17:52:45 +0530

    My commit

---
+++ project/models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(1000))
